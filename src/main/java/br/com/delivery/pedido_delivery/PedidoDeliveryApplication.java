02d2631ef6d58458865a88910d0c59598b591829
Celson Jardim <celsonjardim.dev@outlook.com>
2025-12-18 21:57:52 -0300

    inicia o projeto

---
+++ src/main/java/br/com/delivery/pedido_delivery/PedidoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.delivery.pedido_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/delivery/pedido_delivery/PedidoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivery.pedido_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
