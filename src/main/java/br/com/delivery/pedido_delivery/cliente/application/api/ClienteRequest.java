d93b0b9bc03fb396b958e60df8e3242341af8277
Celson Jardim <celsonjardim.dev@outlook.com>
2025-12-29 20:01:49 -0300

    Implementa request e response

--- src/main/java/br/com/delivery/pedido_delivery/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/delivery/pedido_delivery/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,27 @@
 package br.com.delivery.pedido_delivery.cliente.application.api;
 
+import br.com.delivery.pedido_delivery.cliente.domain.EnderecoCliente;
+import br.com.delivery.pedido_delivery.cliente.domain.Sexo;
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    @CPF
+    private String cpf;
+    private Sexo sexo;
+    @NotBlank
+    @Embedded
+    private EnderecoCliente enderecoCliente;
+
 }
