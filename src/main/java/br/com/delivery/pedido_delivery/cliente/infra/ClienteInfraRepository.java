c9dd162f26ced3df7fda84f7a845c8fe36069463
Celson Jardim <celsonjardim.dev@outlook.com>
2025-12-30 22:41:25 -0300

    implementa cliente repository

--- src/main/java/br/com/delivery/pedido_delivery/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/delivery/pedido_delivery/cliente/infra/ClienteInfraRepository.java
@@ -2,15 +2,20 @@ package br.com.delivery.pedido_delivery.cliente.infra;
 
 import br.com.delivery.pedido_delivery.cliente.application.repository.ClienteRepository;
 import br.com.delivery.pedido_delivery.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
+    private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
+
     @Override
     public Cliente salva(Cliente cliente) {
         log.info("[start] ClienteInfraRepository - salva");
+        clienteSpringDataJPARepository.save(cliente);
         log.info("[finish] ClienteInfraRepository - salva");
         return cliente;
     }
