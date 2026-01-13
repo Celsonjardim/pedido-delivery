bbcd4712518d6d9fb979be1db619f3692bce17db
Celson Jardim <celsonjardim.dev@outlook.com>
2026-01-12 23:09:16 -0300

    Remove anotacao @JsonPropertyOrder

--- src/main/java/br/com/delivery/pedido_delivery/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/delivery/pedido_delivery/cliente/application/api/ClienteListResponse.java
@@ -3,7 +3,6 @@ package br.com.delivery.pedido_delivery.cliente.application.api;
 import br.com.delivery.pedido_delivery.cliente.domain.Cliente;
 import br.com.delivery.pedido_delivery.cliente.domain.EnderecoCliente;
 import br.com.delivery.pedido_delivery.cliente.domain.Sexo;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Value;
 
 import java.util.List;
@@ -11,7 +10,6 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Value
-@JsonPropertyOrder({"idCliente", "nomeCompleto", "email", "celular", "cpf", "sexo", "enderecoCliente"})
 public class ClienteListResponse {
     private UUID idCliente;
     private String nomeCompleto;
