3ccc2615b1771db42f220428a79b4ac5a7190aac
Celson Jardim <celsonjardim.dev@outlook.com>
2026-01-26 23:11:40 -0300

    Novo construtor de Pedido e adicionarItem

--- src/main/java/br/com/delivery/pedido_delivery/pedido/domain/Pedido.java
+++ src/main/java/br/com/delivery/pedido_delivery/pedido/domain/Pedido.java
@@ -1,6 +1,8 @@
 package br.com.delivery.pedido_delivery.pedido.domain;
 
+import br.com.delivery.pedido_delivery.pedido.application.api.PedidoRequest;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -17,15 +19,32 @@ import java.util.UUID;
 public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idPedido", updatable = false, unique = true, nullable = false)
     private UUID idPedido;
+    @NotNull
+    private UUID idClientePedido;
     @NotBlank
     private String valorDoPedido;
     @Enumerated(EnumType.STRING)
     private StatusPedido statusDoPeido;
     @NotNull
-    @OneToMany
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemPedido> itensDoPedido;
 
     private LocalDateTime dataDoPedido;
     private LocalDateTime dataUltimaAlterecao;
+
+    public Pedido(UUID idCliente, @Valid PedidoRequest pedidoRequest) {
+        this.idClientePedido = idCliente;
+        this.valorDoPedido = pedidoRequest.getValorDoPedido();
+        this.statusDoPeido = pedidoRequest.getStatusDoPeido();
+        this.itensDoPedido = pedidoRequest.getItensDoPedido();
+        this.dataDoPedido = LocalDateTime.now();
+    }
+
+    public void adicionarItem(ItemPedido item) {
+        item.setPedido(this);
+        this.itensDoPedido.add(item);
+    }
+
 }
