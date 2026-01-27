94a8b5c4248e046f34842eb9ebc4f12f1ed1eac9
Celson Jardim <celsonjardim.dev@outlook.com>
2026-01-26 23:12:47 -0300

    corrige mapeamento ItemPedido

--- src/main/java/br/com/delivery/pedido_delivery/pedido/domain/ItemPedido.java
+++ src/main/java/br/com/delivery/pedido_delivery/pedido/domain/ItemPedido.java
@@ -1,9 +1,6 @@
 package br.com.delivery.pedido_delivery.pedido.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
@@ -22,4 +19,10 @@ public class ItemPedido {
     private String quantidade;
     @NotNull
     private String precoUnitario;
+    @ManyToOne
+    @JoinColumn(name = "idPedido")
+    private Pedido pedido;
+
+    public void setPedido(Pedido pedido) {
+    }
 }
