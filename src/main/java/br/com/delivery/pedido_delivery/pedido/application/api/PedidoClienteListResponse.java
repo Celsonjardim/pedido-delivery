f7fe8ad1d2a26daea7c2e8a7727c0659def445f6
Celson Jardim <celsonjardim.dev@outlook.com>
2026-01-27 22:04:17 -0300

    Implementa PedidoClienteListResponse

--- src/main/java/br/com/delivery/pedido_delivery/pedido/application/api/PedidoClienteListResponse.java
+++ src/main/java/br/com/delivery/pedido_delivery/pedido/application/api/PedidoClienteListResponse.java
@@ -1,12 +1,36 @@
 package br.com.delivery.pedido_delivery.pedido.application.api;
 
+import br.com.delivery.pedido_delivery.pedido.domain.ItemPedido;
 import br.com.delivery.pedido_delivery.pedido.domain.Pedido;
+import br.com.delivery.pedido_delivery.pedido.domain.StatusPedido;
+import lombok.Value;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class PedidoClienteListResponse {
+    private UUID idPedido;
+    private UUID idClientePedido;
+    private String valorDoPedido;
+    private StatusPedido statusDoPeido;
+    private List<ItemPedido> itensDoPedido;
+    private LocalDateTime dataDoPedido;
 
     public static List<PedidoClienteListResponse> converte(List<Pedido> pedidoDoCliente) {
-        return null;
+        return pedidoDoCliente.stream()
+                .map(PedidoClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public PedidoClienteListResponse(Pedido pedido) {
+        this.idPedido = pedido.getIdPedido();
+        this.idClientePedido = pedido.getIdClientePedido();
+        this.valorDoPedido = pedido.getValorDoPedido();
+        this.statusDoPeido = pedido.getStatusDoPeido();
+        this.itensDoPedido = pedido.getItensDoPedido();
+        this.dataDoPedido = pedido.getDataDoPedido();
     }
 }
